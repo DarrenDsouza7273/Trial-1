a2fa8145cf852c17dde45a4aa10cea6ddda790d0
Darren Dsouza <darrendsouza7273@gmail.com>
2023-07-25 00:22:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f01cea9c16mshc48e3b23cd5b718p1e38f0jsna3c7f5a5f063',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
